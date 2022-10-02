26cefcbcc90a5d4b2675486dcd5e48a4b894bc57
ZakirovaVera <zakirovavk@gmail.com>
2022-10-02 18:02:12 +0500

    доп методы

--- task1.py
+++ task1.py
@@ -3,12 +3,23 @@
 # Пример:
 # - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 
-numbers_list = [2, 3, 5, 9, 3]
-print(numbers_list)
-amount_on_odd_positions = 0
+# numbers_list = [2, 3, 5, 9, 3]
+# print(numbers_list)
+# amount_on_odd_positions = 0
 
-for i, item in enumerate(numbers_list):
-    if i % 2 == 1:
-        amount_on_odd_positions += item
-        print(f'На позиции {i} -> {item}')
-print(f'Сумма элементов на нечетных позициях = {amount_on_odd_positions}')
+# for i, item in enumerate(numbers_list):
+#     if i % 2 == 1:
+#         amount_on_odd_positions += item
+#         print(f'На позиции {i} -> {item}')
+# print(f'Сумма элементов на нечетных позициях = {amount_on_odd_positions}')
+
+users_nums = [
+    45, # 0
+    67, # 1
+    43, # 2
+    78, # 3
+    3,  # 4
+    5,  # 5
+]
+
+print(f'Sum => {sum(users_nums[1::2])}')
--- task3.py
+++ task3.py
@@ -3,18 +3,24 @@
 # Пример:
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
-numbers = [1.1, 1.2, 3.1, 5, 10.01]
+# numbers = [1.1, 1.2, 3.1, 5, 10.01]
 
-max_value_fractional_part = round(numbers[0] % 1, 2)
-min_value_fractional_part = round(numbers[0] % 1, 2)
+# max_value_fractional_part = round(numbers[0] % 1, 2)
+# min_value_fractional_part = round(numbers[0] % 1, 2)
 
-for item in numbers:
-    fractional_part = item % 1
-    if fractional_part > max_value_fractional_part:
-        max_value_fractional_part = fractional_part
-    if fractional_part < max_value_fractional_part:
-        min_value_fractional_part = fractional_part
-    different_number = round(
-        max_value_fractional_part - min_value_fractional_part, 2)
-print(f'{numbers} \nРазницу между максимальным и минимальным значением дробной\
- части элементов {different_number}')
+# for item in numbers:
+#     fractional_part = item % 1
+#     if fractional_part > max_value_fractional_part:
+#         max_value_fractional_part = fractional_part
+#     if fractional_part < max_value_fractional_part:
+#         min_value_fractional_part = fractional_part
+#     different_number = round(
+#         max_value_fractional_part - min_value_fractional_part, 2)
+# print(f'{numbers} \nРазницу между максимальным и минимальным значением дробной\
+#  части элементов {different_number}')
+
+lst = [1.1, 1.2, 3.1, 5, 10.01]
+
+lst = [round(val % 1, 2) for val in lst]
+rev_result = max(lst) - min(lst)
+print(rev_result)
